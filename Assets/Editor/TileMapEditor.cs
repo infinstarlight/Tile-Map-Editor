d7e87c986d1647af8bcfe20f4cf4331ec795413c
Brandon Jones <bcjones1@fullsail.edu>
2017-03-06 21:19:20 -0500

    Created paint function
    Almost done!

--- Assets/Editor/TileMapEditor.cs
+++ Assets/Editor/TileMapEditor.cs
@@ -1,106 +1,189 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEditor;
 
-[CustomEditor(typeof(TileMap))]
-public class TileMapEditor : Editor {
+[CustomEditor (typeof(TileMap))]
+public class TileMapEditor : Editor
+{
 
 	public TileMap map;
 
-    TileBrush brush;
+	TileBrush brush;
+	Vector3 mouseHitPos;
 
+	bool mouseOnMap {
+		get { return mouseHitPos.x > 0 && mouseHitPos.x < map.gridSize.x && mouseHitPos.y < 0 && mouseHitPos.y > -map.gridSize.y; }
+	}
 
-	public override void OnInspectorGUI()
+	public override void OnInspectorGUI ()
 	{
-		EditorGUILayout.BeginVertical();
+		EditorGUILayout.BeginVertical ();
 
-        var oldSize = map.mapSize;
-		map.mapSize = EditorGUILayout.Vector2Field ("Map Size", map.mapSize);
-        if(map.mapSize != oldSize)
-        {
-            UpdateCalculations();
-        }
+		var oldSize = map.mapSize;
+		map.mapSize = EditorGUILayout.Vector2Field ("Map Size:", map.mapSize);
+		if (map.mapSize != oldSize) {
+			UpdateCalculations ();
+		}
 
 		map.texture2D = (Texture2D)EditorGUILayout.ObjectField ("Texture2D:", map.texture2D, typeof(Texture2D), false);
 
 		if (map.texture2D == null) {
-			EditorGUILayout.HelpBox ("A texture 2D has not been selected.", MessageType.Warning);
-		} 
-		else 
-		{
-			EditorGUILayout.LabelField("Tile Size: ", map.tileSize.x+"x"+map.tileSize.y);
-            EditorGUILayout.LabelField("Grid Size In Units: ", map.gridSize.x+"x"+map.gridSize.y);
-            EditorGUILayout.LabelField("Pixel To Units: ", map.pixelsToUnits.ToString());
+			EditorGUILayout.HelpBox ("You have not selected a texture 2D yet.", MessageType.Warning);
+		} else {
+			EditorGUILayout.LabelField ("Tile Size:", map.tileSize.x + "x" + map.tileSize.y);
+			EditorGUILayout.LabelField ("Grid Size In Units:", map.gridSize.x + "x" + map.gridSize.y);
+			EditorGUILayout.LabelField ("Pixels To Units:", map.pixelsToUnits.ToString ());
+			UpdateBrush (map.currentTileBrush);
 		}
 
 		EditorGUILayout.EndVertical ();
 	}
 
-	void OnEnable()
+	void OnEnable ()
 	{
 		map = target as TileMap;
 		Tools.current = Tool.View;
 
-        if (map.texture2D != null)
-        {
-            UpdateCalculations();
-            NewBrush();
-        }
+		if (map.tiles == null) {
+			var go = new GameObject ("Tiles");
+			go.transform.SetParent (map.transform);
+			go.transform.position = Vector3.zero;
 
-    }
+			map.tiles = go;
+		}
 
-    void OnDisable()
-    {
-        DestroyBrush();
-    }
-
-    void UpdateCalculations()
-        {
-            var path = AssetDatabase.GetAssetPath(map.texture2D);
-            map.spriteReferences = AssetDatabase.LoadAllAssetsAtPath(path);
-
-            var sprite = (Sprite)map.spriteReferences[1];
-            var width = sprite.textureRect.width;
-            var height = sprite.textureRect.height;
-
-            map.tileSize = new Vector2(width, height);
-            map.pixelsToUnits = (int)(sprite.rect.width / sprite.bounds.size.x);
-            map.gridSize = new Vector2((width / map.pixelsToUnits) * map.mapSize.x, (height / map.pixelsToUnits) * map.mapSize.y);
-        
+		if (map.texture2D != null) {
+			UpdateCalculations ();
+			NewBrush ();
+		}
 	}
 
-    void CreateBrush()
-    {
-        var sprite = map.currentTileBrush;
-        if(sprite != null)
-        {
-            GameObject go = new GameObject("Brush");
-            go.transform.SetParent(map.transform);
+	void OnDisable ()
+	{
+		DestroyBrush ();
+	}
 
-            brush = go.AddComponent<TileBrush>();
-            brush.renderer2D = go.AddComponent<SpriteRenderer>();
+	void OnSceneGUI ()
+	{
+		if (brush != null) {
+			UpdateHitPosition ();
+			MoveBrush ();
 
-            var pixelsToUnits = map.pixelsToUnits;
-            brush.brushSize = new Vector2(sprite.textureRect.width / pixelsToUnits, sprite.textureRect.height / pixelsToUnits);
+			if (map.texture2D != null && mouseOnMap) {
+				Event current = Event.current;
+				if (current.shift) {
+					Draw ();
+				}
+			}
+		}
+	}
 
-            brush.UpdateBrush(sprite);
-        }
-    }
+	void UpdateCalculations ()
+	{
+		var path = AssetDatabase.GetAssetPath (map.texture2D);
+		map.spriteReferences = AssetDatabase.LoadAllAssetsAtPath (path);
 
-    void NewBrush()
-    {
-        if(brush == null)
-        {
-            CreateBrush();
-        }
-    }
+		var sprite = (Sprite)map.spriteReferences [1];
+		var width = sprite.textureRect.width;
+		var height = sprite.textureRect.height;
 
-    void DestroyBrush()
-    {
-        if (brush != null)
-        {
-            DestroyImmediate(brush.gameObject);
-        }
-    }
+		map.tileSize = new Vector2 (width, height);
+		map.pixelsToUnits = (int)(sprite.rect.width / sprite.bounds.size.x);
+		map.gridSize = new Vector2 ((width / map.pixelsToUnits) * map.mapSize.x, (height / map.pixelsToUnits) * map.mapSize.y);
+
+	}
+
+	void CreateBrush ()
+	{
+
+		var sprite = map.currentTileBrush;
+		if (sprite != null) {
+			GameObject go = new GameObject ("Brush");
+			go.transform.SetParent (map.transform);
+
+			brush = go.AddComponent<TileBrush> ();
+			brush.renderer2D = go.AddComponent<SpriteRenderer> ();
+
+			var pixelsToUnits = map.pixelsToUnits;
+			brush.brushSize = new Vector2 (sprite.textureRect.width / pixelsToUnits,
+				sprite.textureRect.height / pixelsToUnits);
+			brush.UpdateBrush (sprite);
+		}
+	}
+
+	void NewBrush ()
+	{
+		if (brush == null)
+			CreateBrush ();
+	}
+
+	void DestroyBrush ()
+	{
+		if (brush != null)
+			DestroyImmediate (brush.gameObject);
+	}
+
+	public void UpdateBrush (Sprite sprite)
+	{
+		if (brush != null)
+			brush.UpdateBrush (sprite);
+	}
+
+	void UpdateHitPosition ()
+	{
+
+		var p = new Plane (map.transform.TransformDirection (Vector3.forward), Vector3.zero);
+		var ray = HandleUtility.GUIPointToWorldRay (Event.current.mousePosition);
+		var hit = Vector3.zero;
+		var dist = 0f;
+
+		if (p.Raycast (ray, out dist))
+			hit = ray.origin + ray.direction.normalized * dist;
+
+		mouseHitPos = map.transform.InverseTransformPoint (hit);
+
+	}
+
+	void MoveBrush ()
+	{
+		var tileSize = map.tileSize.x / map.pixelsToUnits;
+
+		var x = Mathf.Floor (mouseHitPos.x / tileSize) * tileSize;
+		var y = Mathf.Floor (mouseHitPos.y / tileSize) * tileSize;
+
+		var row = x / tileSize;
+		var column = Mathf.Abs (y / tileSize) - 1;
+
+		if (!mouseOnMap)
+			return;
+
+		var id = (int)((column * map.mapSize.x) + row);
+
+		brush.tileID = id;
+
+		x += map.transform.position.x + tileSize / 2;
+		y += map.transform.position.y + tileSize / 2;
+
+		brush.transform.position = new Vector3 (x, y, map.transform.position.z);
+	}
+
+	void Draw ()
+	{
+		var id = brush.tileID.ToString ();
+
+		var posX = brush.transform.position.x;
+		var posY = brush.transform.position.y;
+
+		GameObject tile = GameObject.Find (map.name + "/Tiles/tiles_" + id);
+
+		if (tile == null) {
+			tile = new GameObject ("tile_" + id);
+			tile.transform.SetParent (map.tiles.transform);
+			tile.transform.position = new Vector3 (posX, posY, 0);
+			tile.AddComponent<SpriteRenderer> ();
+		}
+
+		tile.GetComponent<SpriteRenderer> ().sprite = brush.renderer2D.sprite;
+	}
+
 }
--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -1,67 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class TileMap : MonoBehaviour {
+public class TileMap : MonoBehaviour
+{
 
-    public Vector2 mapSize = new Vector2(20, 10);
-    public Texture2D texture2D;
-    public Vector2 tileSize = new Vector2();
-    public Object[] spriteReferences;
-    public Vector2 gridSize = new Vector2();
-    public int pixelsToUnits = 100;
-    public int tileID = 0;
+	public Vector2 mapSize = new Vector2 (20, 10);
+	public Texture2D texture2D;
+	public Vector2 tileSize = new Vector2 ();
+	public Object[] spriteReferences;
+	public Vector2 gridSize = new Vector2 ();
+	public int pixelsToUnits = 100;
+	public int tileID = 0;
+	public GameObject tiles;
 
-    public Sprite currentTileBrush
-    {
-        get {return spriteReferences[tileID] as Sprite;}
-    }
+	public Sprite currentTileBrush {
+		get { return spriteReferences [tileID] as Sprite; }
+	}
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
 	}
 
-     void OnDrawGizmosSelected()
-    {
-        var pos = transform.position;
+	void OnDrawGizmosSelected ()
+	{
+		var pos = transform.position;
 
-        if(texture2D != null)
-        {
+		if (texture2D != null) {
 
-            Gizmos.color = Color.gray;
-            var row = 0;
-            var maxColumns = mapSize.x;
-            var total = mapSize.x * mapSize.y;
-            var tile = new Vector3(tileSize.x / pixelsToUnits, tileSize.y / pixelsToUnits);
-            var offset = new Vector2(tile.x / 2, tile.y / 2);
+			Gizmos.color = Color.gray;
+			var row = 0;
+			var maxColumns = mapSize.x;
+			var total = mapSize.x * mapSize.y;
+			var tile = new Vector3 (tileSize.x / pixelsToUnits, tileSize.y / pixelsToUnits);
+			var offset = new Vector2 (tile.x / 2, tile.y / 2);
 
-            for (var i = 0; i < total; i++)
-            {
-                var column = i % maxColumns;
+			for (var i = 0; i < total; i++) {
 
-                var newX = (column * tile.x) + offset.x + pos.x;
-                var newY = -(row * tile.y) - offset.y + pos.y;
+				var column = i % maxColumns;
 
-                Gizmos.DrawWireCube(new Vector2(newX, newY), tile);
+				var newX = (column * tile.x) + offset.x + pos.x;
+				var newY = -(row * tile.y) - offset.y + pos.y;
 
-                if(column == maxColumns - 1)
-                {
-                    row++;
-                }
-            }
+				Gizmos.DrawWireCube (new Vector2 (newX, newY), tile);
 
-            Gizmos.color = Color.white;
-            var centerX = pos.x + (gridSize.y / 2);
-            var centerY = pos.y - (gridSize.y / 2);
+				if (column == maxColumns - 1) {
+					row++;
+				}
 
-            Gizmos.DrawWireCube(new Vector2(centerX, centerY), gridSize);
-        }
+			}
 
-    }
+
+			Gizmos.color = Color.white;
+			var centerX = pos.x + (gridSize.x / 2);
+			var centerY = pos.y - (gridSize.y / 2);
+
+			Gizmos.DrawWireCube (new Vector2 (centerX, centerY), gridSize);
+		}
+
+
+	}
+
 }
